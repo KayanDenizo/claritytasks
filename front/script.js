d0c36e127c4304e1783eaa4b8268f57a887c2f91
kayandenizo <kayan.denizo@gmail.com>
2026-05-07 20:09:13 -0300

    feat(front): criando função para cadastrar usuario, limpar formulario e salvar no localstorage

---
+++ front/script.js
@@ -0,0 +1,22 @@
+const form = document.querySelector("#form-cadastro")
+let inputEmail = document.querySelector("#email");
+let inputNome = document.querySelector("#name");
+let inputSenha = document.querySelector("#password");
+let btnCadastro = document.querySelector("#btn-cadastrar")
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault() //previnir que recarregue a pagina
+
+    const usuario = {
+        nome: inputNome.value,
+        email: inputEmail.value,
+        senha: inputSenha.value
+    }
+
+    localStorage.setItem('usuario', JSON.stringify(usuario))
+
+    form.reset()
+    window.location.href = 'signin.html'
+})
+
