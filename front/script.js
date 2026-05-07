d6eb7ebae22ff10aa9ea7784bd5005736889e69c
kayandenizo <kayan.denizo@gmail.com>
2026-05-07 20:19:49 -0300

    feat(front): criando variavel de usuario salvo

--- front/script.js
+++ front/script.js
@@ -4,19 +4,22 @@ let inputNome = document.querySelector("#name");
 let inputSenha = document.querySelector("#password");
 let btnCadastro = document.querySelector("#btn-cadastrar")
 
-
+// FORMULARIO CADASTRO
 form.addEventListener('submit', (e) => {
     e.preventDefault() //previnir que recarregue a pagina
-
+    
     const usuario = {
         nome: inputNome.value,
         email: inputEmail.value,
         senha: inputSenha.value
     }
-
+    
     localStorage.setItem('usuario', JSON.stringify(usuario))
-
+    
     form.reset()
     window.location.href = 'signin.html'
 })
+
+// FORMULARIO LOGIN
+const usuarioSalvo = JSON.parse(localStorage.getItem('usuario'))
 
